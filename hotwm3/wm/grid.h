d2d7b873b8926ce8fd89257cddaf41c46601f898
Filip Gorny <filip.gorny@gmail.com>
2022-03-04 20:28:35 +0100

    Third iteration

---
+++ hotwm3/wm/grid.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef struct {
+    int x, y;
+} GridPoint;
+
+typedef struct {
+    int resolution;
+} Grid;
+
+Grid grid_create(int resolution);
+
+GridPoint grid_get_nearest_point(Grid grid, int x, int y);
